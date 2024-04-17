48af1806eebb0216baeb95bcf811b39b473bf56b
Duc Long <137598421+duclongt23@users.noreply.github.com>
2024-04-17 11:25:12 +0700

    small fix at return and variable name bai5.cpp

--- PuscCode/bai5.cpp
+++ PuscCode/bai5.cpp
@@ -27,6 +27,7 @@ vector<int> chon_3_ward(unordered_map<int,int>& mp){
         top3Keys.push_back(pairs[i].first);
         mp[pairs[i].first]--;
     }
+    return top3Keys;
 }
 
 void bai5(vector<Pedestrian*>& pedestrians, vector<Ward>& wards){
@@ -35,19 +36,19 @@ void bai5(vector<Pedestrian*>& pedestrians, vector<Ward>& wards){
             unordered_map<int,int> mp; // key la index cua pair , value la gia tri pair.second
     do{
         mp.clear();
-        vector<pair<Ward, int>> WardsCounter  = generatePairs(wards, triple ,single);
+        vector<pair<Ward, int>> WardCounter  = generatePairs(wards, triple ,single);
         for (int i=0;i< WardCounter.size();++i){
             mp[i] = WardCounter[i].second;
         }
         for (auto it : pedestrians){
             if (it->getType() == visitor) {
                 vector<Ward> hisJourney;
-                hisJourney.push_back(WardCounter[chon_1_ward()].first);
+                hisJourney.push_back(WardCounter[chon_1_ward(mp)].first);
                 it->setJourney(hisJourney);
             }
             else {
                 vector<Ward> hisJourney;
-                vector<int> a = chon_3_ward();
+                vector<int> a = chon_3_ward(mp);
                 for (int i=0;i<3;++i){
                     isJourney.push_back(WardCounter[a[i]].first);
                 }
